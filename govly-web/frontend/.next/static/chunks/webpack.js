ef6a9a8338902a831fe1bf013732f45d213b60d4
Shao Zhi <soongshaozhi@gmail.com>
2025-08-17 22:05:21 +0800

    Updated README with video

--- govly-web/frontend/.next/static/chunks/webpack.js
+++ govly-web/frontend/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7d968b7bf3ec2eb1"; }
+/******/ 		__webpack_require__.h = function() { return "e66dbe3cb91282f2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
